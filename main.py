2cbbf4bbed900f9c0b34277bf4fc87ae57a40efe
mattesb98 <mattesb98@unet.univie.ac.at>
2022-03-10 15:16:08 +0100

    - feat: creating client data insertion

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
+# import
+import psycopg2
+import names
+import randominfo as rnd
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# environmental variables
+DB_NAME = "postgres"
+DB_HOST = "localhost"
+DB_USER = "mbarkows"
+DB_PASSWORD = "password"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def name_generator():
+
+    # creating first- & lastnames
+    firstname = names.get_first_name()
+    lastname = names.get_last_name()
+
+    print("firstname: ", firstname)
+    print("lastname: ", lastname)
+
+
+def client_transactions():
+    i = 0
+    Id = [j for j in range(200000, 200800)]
+
+    while (i < 778):
+        # creating first- & lastnames
+        firstname = names.get_first_name()
+        lastname = names.get_last_name()
+
+        # creating random mails
+        mail = f"{firstname}.{lastname}@gmail.com"
+
+        # creating random budget
+        budget = random.randrange(100, 1500, 50)
+
+        # creating random jobs
+        job_list = ["Software Engineer", "DevOps Engineer", "Teacher", "Product Manager", "Project Manager", "HR Specialist"]
+        job = random.choice(job_list)
+
+        # creating random client_Id
+        client_Id = Id[i]
+
+        # connect to database
+        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+
+        # insert data record
+        cur = conn.cursor()
+
+        # execute insert command
+        cur.execute(f"INSERT INTO client VALUES ({client_Id}, '{firstname}', '{lastname}', {budget}, '{job}', '{mail}');")
+        #cur.execute("CREATE TABLE student (id INTEGER , name VARCHAR);")
+        conn.commit()
+        cur.close()
+
+        # close connection
+        conn.close()
+        i += 1
+        print(i)
+
+        continue
+
+    else:
+        return print("fertig")
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    client_transactions()
+    #name_generator()
