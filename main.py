d6157965a26af61b76076c34b1759ff3b36a1513
TorukMakto98 <ma.barkowski@gmail.com>
2022-03-18 14:58:26 +0100

    - fix id bug in sketches xml column

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ import names
 import random
 import pandas
 import json
-import config
 from datetime import datetime, timezone
 from xml.dom import minidom
 
 # environmental variables (mac)
-DB_NAME = "postgres"
-DB_HOST = "localhost"
-DB_USER = "mbarkows"
-DB_PASSWORD = "password"
-
-# environmental variables (windows)
 # DB_NAME = "postgres"
 # DB_HOST = "localhost"
-# DB_USER = "postgres"
-# DB_PASSWORD = "postgres"
+# DB_USER = "mbarkows"
+# DB_PASSWORD = "password"
+
+# environmental variables (windows)
+DB_NAME = "postgres"
+DB_HOST = "localhost"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
 
 
 def testing():
@@ -68,113 +67,6 @@ def testing():
 
     xml_str = root.toprettyxml(indent="\t")
     print(xml_str)
-
-
-def create_tables():
-    conn = None
-    try:
-        # connect to database
-        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-
-        # insert data record
-        cur = conn.cursor()
-
-        # execute insert command
-#        cur.execute((
-#         """
-#         CREATE TABLE client(
-#             client_Id INTEGER PRIMARY KEY,
-#             firstname VARCHAR(70),
-#             lastname VARCHAR(70),
-#             budget FLOAT,
-#             mail_address VARCHAR(80),
-#             job_category VARCHAR(80)
-#         )
-#         """,
-#         """
-#         CREATE TABLE plastic_colors(
-#             plastic_colors_Id INTEGER PRIMARY KEY,
-#             fk_client_Id INTEGER,
-#             name VARCHAR(70),
-#             uv_resistant BOOLEAN,
-#             html_code VARCHAR,
-#             polymer_based BOOLEAN,
-#             costs_per_liter FLOAT,
-#             FOREIGN KEY (fk_client_Id) REFERENCES client(client_Id)
-# )
-#         """,
-#         """
-#         CREATE TABLE artist(
-#             artist_Id INTEGER PRIMARY KEY,
-#             firstname VARCHAR,
-#             lastname VARCHAR,
-#             age INTEGER,
-#             mail VARCHAR,
-#             hourly_fee FLOAT
-#         )
-#         """,
-#         """
-#         CREATE TABLE sketches (
-#             sketches_Id INTEGER PRIMARY KEY,
-#             fk_client_Id INTEGER,
-#             fk_artist_Id INTEGER,
-#             figure_theme VARCHAR,
-#             created_date DATE,
-#             estimated_costs INTEGER,
-#             deadline_for_creation DATE,
-#             gcode_file xml,
-#             FOREIGN KEY (fk_client_Id) REFERENCES client(client_id),
-#             FOREIGN KEY (fk_artist_Id) REFERENCES artist(artist_Id)
-#         )
-#         """,
-#         """
-#         CREATE TABLE order (
-#             order_Id INTEGER PRIMARY KEY,
-#             fk_client_Id INTEGER,
-#             order_value INTEGER,
-#             postcode INTEGER,
-#             payment_information json,
-#             street VARCHAR,
-#             city VARCHAR,
-#             FOREIGN KEY (fk_client_Id) REFERENCES client(client_id),
-#         )
-#         """,
-#         """
-#         CREATE TABLE printer (
-#             printer_Id INTEGER PRIMARY KEY,
-#             fk_client_Id INTEGER,
-#             printer_name VARCHAR,
-#             wlan BOOLEAN,
-#             category VARCHAR,
-#             year_of_creation INTEGER,
-#             supplier json,
-#             FOREIGN KEY (fk_client_Id) REFERENCES client(client_id),
-#         )
-#         """,
-#         """
-#         CREATE TABLE print_job (
-#             print_job_Id INTEGER PRIMARY KEY,
-#             fk_printer_Id INTEGER,
-#             waiting_list_position INTEGER,
-#             current_heat INTEGER,
-#             printing_costs INTEGER,
-#             status VARCHAR,
-#             stl_file xml,
-#             processing_time INTEGER,
-#             FOREIGN KEY (fk_printer_Id) REFERENCES printer(printer_Id),
-#         )
-#         """))
-        cur.execute("CREATE TABLE student (id INTEGER , name VARCHAR);")
-        conn.commit()
-        cur.close()
-
-        # close connection
-        conn.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
 
 
 def client_transactions():
@@ -400,6 +292,7 @@ def sketches_transactions():
     client_id_list = [j for j in range(200000, 200776)]
     #date_time = datetime.now(timezone.utc)
     figure_theme_list = ["action figure", "car", "sword", "weapon", "plane", "building", "tool"]
+    xml_file_id_list = [j for j in range(900000, 900800)]
 
     while (i < 777):
         # define sketches id
@@ -421,6 +314,8 @@ def sketches_transactions():
         time_list = pandas.date_range(start="2018-09-09", end="2022-02-02")
         time = random.choice(time_list)
 
+        # define xml file id
+        xml_file_id = xml_file_id_list[i]
         # create gcode xml
         root = minidom.Document()
 
@@ -428,7 +323,7 @@ def sketches_transactions():
         root.appendChild(xml)
 
         doc = root.createElement('file')
-        doc.setAttribute('id', f"{random.choice([j for j in range(900000, 900800)])}")
+        doc.setAttribute('id', f"{xml_file_id}")
         xml.appendChild(doc)
 
         formatChild = root.createElement('format')
@@ -753,11 +648,10 @@ def print_job_transactions():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #create_tables()
-    #client_transactions()
-    #plastic_colors_transactions()
-    #artist_transactions()
-    #sketches_transactions()
-    #order_transactions()
-    #printer_transactions()
+    client_transactions()
+    plastic_colors_transactions()
+    artist_transactions()
+    sketches_transactions()
+    order_transactions()
+    printer_transactions()
     print_job_transactions()
